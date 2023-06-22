d0faec97a4a34aa24e6cfd81d1b5ac5492885825
Ignacio <ukino22@yahoo.com.ar>
2023-06-21 23:04:37 -0300

    primer commit de representante

---
+++ JS/funciones.js
@@ -0,0 +1,727 @@
+
+function wapp() {
+    window.open('https://api.whatsapp.com/send?phone=54959791914&text=Hola%20Federico.%20Te%20contacto%20por%20las%20las%20clases%20de%20música%20');
+  }  
+
+function darwin() {
+    window.location.href="darwin-nunez.html";
+  }  
+
+function veliz() {
+    window.location.href="alejo-veliz.html";
+  }  
+
+function advincula() {
+    window.location.href="luis-advincula.html";
+  }  
+
+function espinoza() {
+    window.location.href="/cristian-espinoza.html";
+  }  
+
+function zuculini() {
+    window.location.href="/bruno-zuculini.html";
+  }  
+
+function mier() {
+    window.location.href="/kevin-mier.html";
+  }  
+
+function banini() {
+    window.location.href="/estefania-banini.html";
+  }  
+
+function anselmi() {
+    window.location.href="/martin-anselmi.html";
+  }  
+  
+// DARWIN NUÑEZ //
+
+  function func_office() {
+    var elem = document.getElementById("barra_office");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_sumacrm() {
+    var elem = document.getElementById("barra_sumacrm");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_evernote() {
+    var elem = document.getElementById("barra_evernote");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 70) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+  
+  function func_googledocs() {
+    var elem = document.getElementById("barra_googledocs");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_trello() {
+    var elem = document.getElementById("barra_trello");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_ingles() {
+    var elem = document.getElementById("barra_ingles");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  // ALEJO VÉLIZ //
+
+  function func_veliz_ofensiva() {
+    var elem = document.getElementById("barra_office");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_veliz_balon() {
+    var elem = document.getElementById("barra_sumacrm");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_veliz_vision() {
+    var elem = document.getElementById("barra_evernote");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 70) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+  
+  function func_veliz_defensivo() {
+    var elem = document.getElementById("barra_googledocs");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_veliz_aereo() {
+    var elem = document.getElementById("barra_trello");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+  function func_ingles() {
+    var elem = document.getElementById("barra_ingles");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
+
+// LUIS ADVÍNCULA //
+
+function func_advincula_ofensiva() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_advincula_balon() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_advincula_vision() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_advincula_defensivo() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_advincula_aereo() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+// CRISTIAN ESPINOZA //
+
+function func_office() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_sumacrm() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_evernote() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_googledocs() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_trello() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+// BRUNO ZUCULINI //
+
+function func_office() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_sumacrm() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_evernote() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_googledocs() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_trello() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+// KEVIN MIER //
+
+function func_office() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_sumacrm() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_evernote() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_googledocs() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_trello() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+// ESTEFANÍA BANINI //
+
+function func_office() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_sumacrm() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_evernote() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_googledocs() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_trello() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+// MARTÍN ANSELMI //
+
+function func_office() {
+  var elem = document.getElementById("barra_office");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_sumacrm() {
+  var elem = document.getElementById("barra_sumacrm");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 90) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_evernote() {
+  var elem = document.getElementById("barra_evernote");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_googledocs() {
+  var elem = document.getElementById("barra_googledocs");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_trello() {
+  var elem = document.getElementById("barra_trello");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+function func_ingles() {
+  var elem = document.getElementById("barra_ingles");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+
+
---
+++ practicas.py
@@ -0,0 +1,10 @@
+edad = int(input("Por favor, ingrese su edad: "))
+print(edad)
+print("El ano que viene voy a tener" , edad+1)
+
+b = int(input("Ingrese la base: "))
+h = int(input("Ingrese la altura: "))
+
+r = b*h
+
+print("La superficie es:",r ,"m2")
